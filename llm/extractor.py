df5ecaba964e2aa9e383835b105c9c27e84ebc9d
DaddarioLuigi <44373889+DaddarioLuigi@users.noreply.github.com>
2025-06-30 18:05:45 +0200

    update working

--- llm/extractor.py
+++ llm/extractor.py
@@ -12,7 +12,7 @@ TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
 async_client = Together(api_key=TOGETHER_API_KEY)
 
 #si potrebbe anche togliere async
-async def get_response_from_document(document_text: str, document_type: str, model: str) -> str:
+def get_response_from_document(document_text: str, document_type: str, model: str) -> str:
     prompt = get_prompt_for(document_type) + "\n\n" + document_text
     print(get_prompt_for(document_type))
     for sleep_time in [1, 2, 4]:
@@ -28,6 +28,5 @@ async def get_response_from_document(document_text: str, document_type: str, mod
             break
         except error.RateLimitError as e:
             print(e)
-            await asyncio.sleep(sleep_time)
 
     return response.choices[0].message.content
