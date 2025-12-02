a23728888cf1aaddb386e8cf5b951f57ea4bcc7a
DaddarioLuigi <44373889+DaddarioLuigi@users.noreply.github.com>
2025-12-02 18:16:39 +0100

    stage

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ origins = [o.strip() for o in os.getenv("FRONTEND_ORIGINS", "http://localhost:30
 CORS(app, origins=origins, supports_credentials=True)
 
 # Configurazione database PostgreSQL
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:IfVBYLGfZdXEDVYPXWhipcEhIYAHTGyw@switchback.proxy.rlwy.net:51809/railway")
+DATABASE_URL = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
